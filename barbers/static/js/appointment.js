1c9195729458f33b724f94c4286bd4a31252f19a
syadallah <syadallah@gmail.com>
2020-05-25 17:52:08 -0400

    POST method to book appointment
    Update view.py
    Update appointment.js and appointment.html

--- barbers/static/js/appointment.js
+++ barbers/static/js/appointment.js
@@ -1,5 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // ==================== REGISTER ==============================================
+  // ==================== Appointment Form ==============================================
 
     document.querySelector('#submit-form').onclick = () => {
+    const fname = $('#fname').value;
+    const lname = $('#lname').value;
+    const phone = $('#phone').value;
+    const email = $('#email').value;
+    const barber = $('#barber').value;
+    const myDate = $('#myDate').value;
+    const time = $('#time').value;
+    const comment = $('#comment').value;
+
+    let request = new XMLHttpRequest();
+    request.open('POST', '/appointment')
+    var csrf_token = $("[name=csrfmiddlewaretoken]").val();
+    request.setRequestHeader("X-CSRFToken", csrf_token);
+    console.log(csrf_token)
+
+    // The FormData() object can be used to transmit data to the server (ie.
+      // transmit data to views.py).
+      let appointment_data = new FormData();
+      appointment_data.append('fname', fname);
+      appointment_data.append('lname', lname);
+      appointment_data.append('phone', phone);
+      appointment_data.append('email', email);
+      appointment_data.append('barber', barber);
+      appointment_data.append('myDate', myDate);
+      appointment_data.append('time', time);
+      appointment_data.append('comment', comment);
+      console.log(appointment_data)
+
+      // Send register_data (aka FormData()) to views.py, followed by a callback
+        // function that executes once a response is received from views.py.
+        request.send(appointment_data);
+        request.onload = () => {
+          const response = request.responseText;
+          const success = JSON.parse(response)['success'];
+          const error_message = JSON.parse(response)['message'];
+
+        console.log(this.responseText);
+
+       // If views.py returns a JSON response where success = true, then
+       // the page is reloaded in order to redirect the user to index.html.
+       if (success === true) {
+         window.location.href = '/';
     }
+}
+}
+})
--- barbers/views.py
+++ barbers/views.py
@@ -1,8 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from barbers.models import Appointment
 # Create your views here.
 def index(request):
     return render(request, "barbers/index.html")
 def appointment(request):
-    return render(request, "barbers/appointment.html")
+      # POST
+  if request.method == 'POST':
+
+    # Save order data to the database
+    add = Appointment(
+            fname=request.POST.get('fname'),
+            lname=request.POST.get('lname'),
+            phone=request.POST.get('phone'),
+            email=request.POST.get('email'),
+            barber=request.POST.get('barber'),
+            date=request.POST.get('myDate'),
+            time=request.POST.get('time'),
+            comment=request.POST.get('comment')
+          )
+    add.save()
+ # GET
+  else:
+      return render(request, "barbers/appointment.html")
