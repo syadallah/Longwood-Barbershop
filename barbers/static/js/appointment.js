811fb3fd27e32812fdc55ab849c8eb522ccb2495
syadallah <syadallah@gmail.com>
2020-05-25 15:02:03 -0400

    Add appointment.js files
    JavaScript file added to handle insert date to appointment file

---
+++ barbers/static/js/appointment.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // ==================== REGISTER ==============================================
+
+    document.querySelector('#submit-form').onclick = () => {
+    }
