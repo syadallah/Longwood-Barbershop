32c2adbad1116e9692b24a9ee215e2da66faed08
syadallah <syadallah@gmail.com>
2020-05-25 18:01:04 -0400

    Alter Appointment model
    Adding null=True for email, contact, comment, and barber columns

--- barbers/models.py
+++ barbers/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Appointment(models.Model):
   fname = models.CharField(max_length=64)
   lname = models.CharField(max_length=64)
-  barber = models.CharField(max_length=64, blank=True)
+  barber = models.CharField(max_length=64, blank=True, null=True)
   phone = models.CharField(max_length=12)
-  email = models.EmailField(max_length=254, blank=True)
-  contact = models.CharField(max_length=64, blank=True)
+  email = models.EmailField(max_length=254, blank=True, null=True)
+  contact = models.CharField(max_length=64, blank=True, null=True)
   date = models.DateField()
   time = models.CharField(max_length=64)
-  comment = models.TextField(blank=True)
+  comment = models.TextField(blank=True, null=True)
 
   def __str__(self):
     return f"{self.fname}, {self.lname},{self.barber},{self.phone},{self.email},{self.contact},{self.date},{self.time}, {self.comment}"
