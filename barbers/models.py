7c8c7a1cad3f05aff3ad832f70468470c3da3888
syadallah <syadallah@gmail.com>
2020-05-24 20:30:04 -0400

    Add appointment model

--- barbers/models.py
+++ barbers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Appointment(models.Model):
+  fname = models.CharField(max_length=64)
+  lname = models.CharField(max_length=64)
+  barber = models.CharField(max_length=64, blank=True)
+  phone  = models.PositiveIntegerField(blank=True)
+  email     = models.EmailField(max_length=254, blank=True)
+  contact = models.CharField(max_length=64, blank=True)
+  date  = models.DateField()
+  time = models.CharField(max_length=64)
+  comment = models.TextField(blank=True)
+
+  def __str__(self):
+    return f"{self.fname}, {self.lname},{self.barber},{self.phone},{self.email},{self.contact},{self.date},{self.time}, {self.comment}"
