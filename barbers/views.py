ab0de3be7d28a5b6ae849bff6d29b10c7b0099cd
syadallah <syadallah@gmail.com>
2020-05-25 18:54:30 -0400

    Insert data to PostgreSQL using view.py
    Send response to front end using HttpResponse

--- barbers/views.py
+++ barbers/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from barbers.models import Appointment
@@ -7,19 +9,25 @@ def index(request):
 def appointment(request):
       # POST
   if request.method == 'POST':
-
-    # Save order data to the database
+      # Save order data to the database
     add = Appointment(
-            fname=request.POST.get('fname'),
-            lname=request.POST.get('lname'),
-            phone=request.POST.get('phone'),
-            email=request.POST.get('email'),
-            barber=request.POST.get('barber'),
-            date=request.POST.get('myDate'),
-            time=request.POST.get('time'),
-            comment=request.POST.get('comment')
-          )
+              fname=request.POST.get('fname'),
+              lname=request.POST.get('lname'),
+              phone=request.POST.get('phone'),
+              email=request.POST.get('email'),
+              barber=request.POST.get('barber'),
+              date=request.POST.get('myDate'),
+              time=request.POST.get('time'),
+              comment=request.POST.get('comment'),
+            )
+
     add.save()
+
+    if add is not None:
+      return HttpResponse('{"success": true, "message": ""}')
+    else:
+      return HttpResponse('{"success": false, "message": "Invalid username and/or password."}')
+
  # GET
   else:
-      return render(request, "barbers/appointment.html")
+    return render(request, "barbers/appointment.html")
