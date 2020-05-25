397df6d4755930f233c1f2ac9dcff1fa027a1922
syadallah <syadallah@gmail.com>
2020-05-24 20:43:00 -0400

    Update appointment model
    Phone changed from Integer to Charfield

--- barbers/models.py
+++ barbers/models.py
@@ -5,10 +5,10 @@ class Appointment(models.Model):
   fname = models.CharField(max_length=64)
   lname = models.CharField(max_length=64)
   barber = models.CharField(max_length=64, blank=True)
-  phone  = models.PositiveIntegerField(blank=True)
-  email     = models.EmailField(max_length=254, blank=True)
+  phone = models.CharField(max_length=12)
+  email = models.EmailField(max_length=254, blank=True)
   contact = models.CharField(max_length=64, blank=True)
-  date  = models.DateField()
+  date = models.DateField()
   time = models.CharField(max_length=64)
   comment = models.TextField(blank=True)
 
